d1e5a62d9e6b119663160c5b2cb8c0a5791c5a94
ismiwrdnta <ismiwrdnta@gmail.com>
2022-03-19 16:39:27 +0700

    menambahkan class SimpleDataBase

--- modul4_1302201135/modul4_1302201135/Program.cs
+++ modul4_1302201135/modul4_1302201135/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace modul4KPL_1302201135
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<double> dataBaru = new SimpleDataBase<double>();
+            dataBaru.SimpleDatabase();
+            dataBaru.AddNewData(13);
+            dataBaru.AddNewData(02);
+            dataBaru.AddNewData(20);
+            dataBaru.PrintAllData();
+
+
+        }
+    }
+}
